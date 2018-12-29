0f0bb80e9a81818b69dce679b09f7df696d4697e
Will Shaughnssy <WillShaughnessy@MacBook-Pro.local>
2018-12-29 17:30:42 -0500

    created separate data file in js

---
+++ data.js
@@ -0,0 +1,87 @@
+
+
+let categoryOptions = {
+    eat: [
+        'Restaurants',
+        'Foodtrucks'
+    ],
+    drink: [
+        'Bars',
+        'Breweries',
+    ],
+}
+
+let restaurantTypes = {
+    'Diner': 101,
+    'Fine Dining': 18,
+    'Pizzeria': 275,
+    'Deli': 24,
+    'Casual Dining': 16,
+    'Cafe': 1,
+    'Sandwhich Shop': 271,
+    'Quick Bites': 21
+}
+
+let foodtruckTypes = {
+    'Foodtrucks': 81
+}
+
+
+let barTypes = {
+    'Wine Bar': 278,
+    'Lounge': 5,
+    'Pub': 6,
+    'Bar': 7
+}
+
+let breweryTypes = {
+    'Brewery': 283,
+    'Microbrewery': 161
+}
+
+
+
+// let categoryOptions = {
+//     eat: {
+//         'Restaurants': {
+
+//             types: {
+//                 'Diner': 101,
+//                 'Fine Dining': 18,
+//                 'Pizzeria': 275,
+//                 'Deli': 24,
+//                 'Casual Dining': 16,
+//                 'Cafe': 1,
+//                 'Sandwhich Shop': 271,
+//                 'Quick Bites': 21
+//             }
+//         },
+//         'Foodtrucks': {
+//             types: {
+//                 'Foodtrucks': 81
+//             }
+//         }
+//     },
+//     drink: {
+//         'Bars': {
+//             types: {
+//                 'Wine Bar': 278,
+//                 'Lounge': 5,
+//                 'Pub': 6,
+//                 'Bar': 7
+//             }
+//         }, 
+//         'Breweries': {
+//             types: {
+//                 'Brewery': 283,
+//                 'Microbrewery': 161
+//             },
+//         }
+//         // 'Events'
+//     },
+//     do: [
+//         'Sports', 
+//         'Concerts', 
+//         'Shopping'
+//     ]
+// }
