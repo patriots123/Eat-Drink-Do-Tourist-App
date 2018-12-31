6883b1e7b655ae4c5745d919f5b70886437a7878
Will Shaughnssy <WillShaughnessy@MacBook-Pro.local>
2018-12-30 20:21:16 -0500

    Added API call to get specific city id for Eat workflow

--- data.js
+++ data.js
@@ -1,117 +1,41 @@
 
-
-// let categoryOptions = {
-//     eat: [
-//         'Restaurants',
-//         'Foodtrucks'
-//     ],
-//     drink: [
-//         'Bars',
-//         'Breweries',
-//     ],
-// }
-
-// let restaurantTypes = {
-//     'Diner': 101,
-//     'Fine Dining': 18,
-//     'Pizzeria': 275,
-//     'Deli': 24,
-//     'Casual Dining': 16,
-//     'Cafe': 1,
-//     'Sandwhich Shop': 271,
-//     'Quick Bites': 21
-// }
-
-// let foodtruckTypes = {
-//     'Foodtrucks': 81
-// }
-
-
-// let barTypes = {
-//     'Wine Bar': 278,
-//     'Lounge': 5,
-//     'Pub': 6,
-//     'Bar': 7
-// }
-
-// let breweryTypes = {
-//     'Brewery': 283,
-//     'Microbrewery': 161
-// }
-
-
 const categoryOptions = {
     eat: {
-            'Restaurants': {
-                'Diner': 101,
-                'Fine Dining': 18,
-                'Pizzeria': 275,
-                'Deli': 24,
-                'Casual Dining': 16,
-                'Cafe': 1,
-                'Sandwhich Shop': 271,
-                'Quick Bites': 21
-            },
-            'Foodtrucks': {
-                'Foodtrucks': 81,
-            }
+        'Restaurants': {
+            'Diner': 101,
+            'Fine Dining': 18,
+            'Pizzeria': 275,
+            'Deli': 24,
+            'Casual Dining': 16,
+            'Cafe': 1,
+            'Sandwhich Shop': 271,
+            'Quick Bites': 21
         },
-    drink: {
-        'Bars': {
-            'Wine Bar': 278,
-            'Lounge': 5,
-            'Pub': 6,
-            'Bar': 7
-        },
-        'Breweries': {
-            'Brewery': 283,
-            'Microbrewery': 161
+        'Foodtrucks': {
+            'Foodtrucks': 81,
         }
-        },
+    },
+    drink: {
+        'Bars': [
+            'Beer Bar',
+            'Brewpub'
+        ],
+        'Breweries': [
+            'Brewery',
+            'Beer Store'
+        ]
+    },
     do: {}
 }
 
-
-
-// const categoryOptions = {
-//         eat: [
-//             {
-//                 categoryType: 'Restaurants',
-//                 types: {
-//                     'Diner': 101,
-//                     'Fine Dining': 18,
-//                     'Pizzeria': 275,
-//                     'Deli': 24,
-//                     'Casual Dining': 16,
-//                     'Cafe': 1,
-//                     'Sandwhich Shop': 271,
-//                     'Quick Bites': 21
-//                 }
-//             },
-//             {
-//                 categoryType: 'Foodtrucks',
-//                 types: {
-//                     'Foodtrucks': 81,
-//                 }
-//             }
-//         ],
-//         drink: [
-//             {
-//                 categoryType: 'Bars',
-//                 types: {
-//                     'Wine Bar': 278,
-//                     'Lounge': 5,
-//                     'Pub': 6,
-//                     'Bar': 7
-//                 }
-//             },
-//             {
-//                 categoryType: 'Breweries',
-//                 types: {
-//                     'Brewery': 283,
-//                     'Microbrewery': 161
-//                 }
-//             }
-//         ],
-//         do: []
-// }
+const apiInfo = {
+        zomato: {
+            key: '67efe258063ce738fec93f327d85c4ab',
+            searchBaseUrl: 'https://developers.zomato.com/api/v2.1/search',
+            citiesBaseUrl: 'https://developers.zomato.com/api/v2.1/cities'
+        },
+        beerMapping: {
+            key: '76f32d8d9bfc0947e2c5af1270c13c76',
+            loccityUrl: 'http://beermapping.com/webservice/loccity/'
+        }
+    }
--- index.js
+++ index.js
@@ -2,12 +2,45 @@
 let cityInput;
 let stateInput;
 let dateInput;
+let zomatoCityID;
 
 let categoryChoice;
 let categoryOptionChoice;
 let categoryOptionTypeChoice;
 
+function matchCityFromAPI(responseJson) {
+    for (let i = 0; i < responseJson.location_suggestions.length; i++) {
+    console.log(responseJson.location_suggestions[i].name);
+        if (responseJson.location_suggestions[i].name == `${cityInput}, ${stateInput}`) {
+            zomatoCityID = responseJson.location_suggestions[i].id;
+            console.log(`Zomato CityID: ${zomatoCityID}`);
+            break;
+        } else {
+            console.log('not a match');
+        }
+    }
+}
 
+function getZomatoCityID() {
+    const params = {
+        q: cityInput,
+    };
+    const queryString = formatQueryParams(params)
+    const requestUrl = citiesBaseUrl + '?' + queryString;
+
+    const options = {
+        headers: new Headers({
+          "user-key": apiInfo.zomato.key,
+        })
+    };
+    fetch(requestUrl, options)
+    .then(response => response.json())
+    .then(responseJson => cityID = matchCityFromAPI(responseJson))
+    // .catch(console.log(`Something went wrong`));
+}
+
+
+// save city, state, and date when the user hits submit and change pages
 function handleLocationDateInput() {
     $('#location-data-entry').submit('click', event => {
         event.preventDefault();
@@ -18,13 +51,14 @@ function handleLocationDateInput() {
         console.log(`city: ${cityInput}, state: ${stateInput}, date: ${dateInput}`);
         $('.home-page').hide();
         $('.eat-drink-do-selection-page').show();
+        getZomatoCityID();
     });
 }
 
 
 
 
-
+//generate HTML for category options (ex. Restaurants/ Foodtrucks for Eat)
 function generateCategoryOptions() {
     let keysForButtonTitles = Object.keys(categoryOptions[categoryChoice]);
     for (let i = 0; i < keysForButtonTitles.length; i++) {
@@ -34,6 +68,7 @@ function generateCategoryOptions() {
     $('.category-options').show();
 }
 
+//After user clicks Eat, Drink, or Do, save that choice and call function to generate the next option buttons
 function handleCategoryDecision() {
     $('.eat-drink-do-selection-page').on('click', '.category-button',function() {
         console.log('category decision made');
@@ -46,18 +81,26 @@ function handleCategoryDecision() {
 
 
 
-
+//generate HTML for category option types (ex. Casual Dining under Restaurants)
 function generateCategoryOptionTypes() {
     $('.category-option-types').empty();
-    let keysForButtonTitles = Object.keys(categoryOptions[categoryChoice][categoryOptionChoice])
-    for (let i = 0; i < keysForButtonTitles.length; i++) {
-        $('.category-option-types').append(
-            `<button id='${keysForButtonTitles[i]}' class='category-option-type-button'>${keysForButtonTitles[i]}</button>`)
+    console.log(categoryChoice);
+    if (categoryChoice == 'eat') {
+        let keysForButtonTitles = Object.keys(categoryOptions[categoryChoice][categoryOptionChoice])
+        for (let i = 0; i < keysForButtonTitles.length; i++) {
+            $('.category-option-types').append(
+                `<button id='${keysForButtonTitles[i]}' class='category-option-type-button'>${keysForButtonTitles[i]}</button>`)
+        }
+    } else if (categoryChoice == 'drink') {
+        for (let i = 0; i < categoryOptions[categoryChoice][categoryOptionChoice].length; i++) {
+            $('.category-option-types').append(
+                `<button id='${categoryOptions[categoryChoice][categoryOptionChoice][i]}' class='category-option-type-button'>${categoryOptions[categoryChoice][categoryOptionChoice][i]}</button>`)
+        }
     }
     $('.category-options').show();
 }
 
-
+//After user clicks one of the category option buttons, call function to generate last set of decision buttons
 function handleCategoryOptionDecision() {
     $('.category-options').on('click', '.category-option-button', function() {
         console.log('category option decision made');
@@ -76,51 +119,86 @@ function formatQueryParams(params) {
     return queryItems.join('&');
 }
 
-function displayResultsContent(responseJson) {
-    console.log('display content');
+function displayEatResults(responseJson) {
+    console.log(categoryOptionChoice);
     $('.search-results-list').empty();
     for (let i = 0; i < responseJson.restaurants.length; i++) {
-        $('.search-results-list').append(
-            `<li class="result-list-item">
-                <p>${responseJson.restaurants[i].restaurant.name}</p>
-                <p>${responseJson.restaurants[i].restaurant.location.address}</p>
-                <p>${responseJson.restaurants[i].restaurant.user_rating.aggregate_rating}</p>
-            </li>`
-        );
+        if (categoryOptionChoice === 'Restaurants') {
+            $('.search-results-list').append(
+                `<li class="result-list-item">
+                    <p>${responseJson.restaurants[i].restaurant.name}</p>
+                    <p>${responseJson.restaurants[i].restaurant.location.address}</p>
+                    <p>${responseJson.restaurants[i].restaurant.user_rating.aggregate_rating}</p>
+                </li>`
+            );
+        } else if (categoryOptionChoice === 'Foodtrucks') {
+            $('.search-results-list').append(
+                `<li class="result-list-item">
+                    <p>${responseJson.restaurants[i].restaurant.name}</p>
+                    <p>${responseJson.restaurants[i].restaurant.location.locality_verbose}</p>
+                    <p>${responseJson.restaurants[i].restaurant.user_rating.aggregate_rating}</p>
+                </li>`
+            );
+        }
     }
     $('.search-results-section').show();
 }
 
-function callEatDrinkAPI() {
-    const apiKey = '67efe258063ce738fec93f327d85c4ab';
-    const baseUrl = 'https://developers.zomato.com/api/v2.1/search';
+function callEatAPI() {
     const params = {
-        q: cityInput,
+        city_id: zomatoCityID,
         count: 15,
         sort: 'rating',
         establishment_type: categoryOptions[categoryChoice][categoryOptionChoice][categoryOptionTypeChoice],
     };
-
     const queryString = formatQueryParams(params)
-    const requestUrl = baseUrl + '?' + queryString;
-
+    const requestUrl = apiInfo.zomato.searchBaseUrl + '?' + queryString;
+    console.log(requestUrl);
     const options = {
         headers: new Headers({
-          "user-key": apiKey,
+          "user-key": apiInfo.zomato.key,
         })
     };
-
-    console.log('calling eat drink API');
+    console.log('calling eat API');
     fetch(requestUrl, options)
     .then(response => response.json())
-    .then(responseJson => displayResultsContent(responseJson))
+    .then(responseJson => displayEatResults(responseJson))
+    // .catch(console.log(`Something went wrong`));
+}
+
+function displayDrinkResults(responseJson) {
+    $('.search-results-list').empty();
+    for (let i = 0; i < responseJson.length; i++) {
+        if (responseJson[i].status ===  categoryOptionTypeChoice) {
+            $('.search-results-list').append(
+                `<li class="result-list-item">
+                    <p>${responseJson[i].name}</p>
+                    <p>${responseJson[i].street} ${responseJson[i].city}, ${responseJson[i].state}</p>
+                    <p>${responseJson[i].overall}</p>
+                </li>`
+            );}
+    }
+    $('.search-results-section').show();
+}
+
+
+
+function callDrinkAPI() {
+    const requestUrl = apiInfo.beerMapping.loccityUrl + `${apiInfo.beerMapping.key}/${cityInput},${stateInput}&s=json`
+    console.log(requestUrl);
+    console.log('calling drink API');
+    fetch(requestUrl)
+    .then(response => response.json())
+    .then(responseJson => displayDrinkResults(responseJson))
     // .catch(console.log(`Something went wrong`));
 }
 
 
 function getResultContent() {
-    if ( categoryChoice === 'eat' || categoryChoice === 'drink') {
-        callEatDrinkAPI();
+    if ( categoryChoice === 'eat') {
+        callEatAPI();
+    } else if (categoryChoice === 'drink') {
+        callDrinkAPI();
     } else {
         // Do
     }
@@ -131,7 +209,6 @@ function handleCategoryOptionTypeDecision() {
         console.log('category option type decision made');
         categoryOptionTypeChoice = $(this).attr('id');
         console.log(categoryOptionTypeChoice);
-        // callEatDrinkAPI();
         getResultContent();
     });
 }
